95b28af408b60e694d5d98ebf8567dbc554ade71
webYuchangxu@163.com <webYuchangxu@163.com>
2022-07-19 00:20:26 +0800

    自我评价增加逐字展示模拟打字，切换页签中断，后续继续打字。

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -24,6 +24,9 @@ Page({
       value: '20W+/年'
     }],
     selfAssessment: '简单的介绍一下我自己，我叫于长旭，93年生人，三口之家。毕业于中北大学。现就职于中软国际有限公司，担任前端开发工程师。年薪20以上，由于工作城市和实际需求，目前并没有准备车和房子。性格方面，大多时候我是偏外向一点的，为人比较好相处，朋友对我的评价也是说比较稳重。生活中，我是一个喜欢探索新鲜事物的人，周末偶尔会去想要去的地方用来放松自己的心情。有时也会宅在家里安静的看看书，追追剧，充实自己。自我评价的话，我觉得自己还是一个比较稳重，热爱生活，能力较强，对未来也充满期待的人。',
+    selfAssessmentStrIndex: 0,
+    selfAssessmentStr: '',
+    strInterval: 0,
     tel: [
       { name: 'chat-o' },
       { name: 'phone-o', value: '17840961025' }
@@ -37,9 +40,28 @@ Page({
   },
   onChange(event: { detail: string }) {
     this.setData({
-      activeName: event.detail
+      activeName: event.detail,
     });
+    const str = this.data.selfAssessment;
+    if (this.data.selfAssessmentStrIndex === str.length) {
+      clearInterval(this.data.strInterval)
+      return
+    }
+    if (event.detail === 'self-assessment') {
+      this.data.strInterval = setInterval(() => {
+        this.setData({
+          selfAssessmentStr: str.slice(0, this.data.selfAssessmentStrIndex)
+        });
+        if (this.data.selfAssessmentStrIndex++ === str.length) {
+          clearInterval(this.data.strInterval)
+        }
+      }, 100)
+      if (this.data.selfAssessmentStr === str) return
+    } else {
+      clearInterval(this.data.strInterval)
+    }
   },
+
   telMe: function (event: { currentTarget: { dataset: { name: any; value: any; }; }; }) {
     const { name } = event.currentTarget.dataset
     switch (name) {
